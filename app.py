ebdc8964a9957bdc943c042a4e166d5e4f7f302c
BRPraveen <137779724+BRPraveen@users.noreply.github.com>
2023-06-26 16:43:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -43,8 +43,8 @@ def main():
 
     # display the front end aspect
     st.markdown(html_temp, unsafe_allow_html = True)
-    st.img('''https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.cnbc.com%2F2019%2F08%2F15%2Fwalmarts-secret-weapon-in-its-quest-to-outmaneuver-amazon.html&psig=AOvVaw042sGEaM1kyLTm7uYcgdxX&ust=1687864167041000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCLjU5ZLm4P8CFQAAAAAdAAAAABAE''')   
-  # following lines create boxes in which user can enter data required to make prediction
+   
+    # following lines create boxes in which user can enter data required to make prediction
     Store = st.number_input("EnterNumber of Store Number",min_value=1, max_value=50)
     Holiday = st.selectbox('Holiday Status',("Holiday","Not Holiday"))
     Temperature = st.number_input("Enter The Temperature value", min_value=1.0, max_value=75.0)
